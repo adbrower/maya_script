5065fa38e902eed79f1ddc06a6680c1b1b082ae2
adbrower <audreydb23@gmail.com>
2019-09-24 23:03:26 -0400

    initialize

---
+++ Adbrower/__init__.py
@@ -0,0 +1,9 @@
+import getpass
+import os
+
+USERNAME = getpass.getuser()
+PATH_WINDOW_INIT = 'C:/Users/' + USERNAME + '/'
+PATH_LINUX_INIT = '/home/' + USERNAME + '/'
+FOLDER_NAME_INIT = '.config/adb_Setup'
+ICONS_FOLDER_INIT = 'C:/Users/Audrey/Google Drive/[SCRIPT]/python/maya_script/Adbrower/adb_icons/'
+
