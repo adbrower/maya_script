1ab017cf208bc2c579fc1f7bb9799f4ee1e7eb7c
adbrower <audreydb23@gmail.com>
2020-02-29 16:13:53 -0500

    added timeit decorator

--- Adbrower/adbrower.py
+++ Adbrower/adbrower.py
@@ -21,7 +21,7 @@
 #    10. Naming
 #
 # ------------------------------------------------------
-
+import time
 import sys
 from functools import wraps
 
@@ -38,6 +38,22 @@ reload(NC)
 # -----------------------------------
 # 2.1 DECORATORS
 # -----------------------------------
+
+def timeit(method):
+    """
+    from: https://medium.com/pythonhive/python-decorator-to-measure-the-execution-time-of-methods-fa04cb6bb36d
+    """
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print 'TIME: %r ----  %2.2f ms' % (method.__name__, (te - ts) * 1000)
+        return result
+    return timed
 
 def repeat(label):       
     """
