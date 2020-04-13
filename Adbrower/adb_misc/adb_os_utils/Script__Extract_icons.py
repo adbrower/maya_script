b7cb071aa54f30450e17f294c2653053186bbd21
adbrower <audreydb23@gmail.com>
2020-04-13 19:02:48 -0400

    update path

--- Adbrower/adb_misc/adb_os_utils/Script__Extract_icons.py
+++ Adbrower/adb_misc/adb_os_utils/Script__Extract_icons.py
@@ -1,6 +1,6 @@
 import maya.cmds as cmds
 
-ICONS_FOLDER = 'C:/Users/Audrey/Google Drive/[SCRIPT]/python/Adbrower/adb_icons/'
+ICONS_FOLDER = 'C:/Users/Audrey/Google Drive/[SCRIPT]/python/maya_script/Adbrower/adb_misc/adb_icons/'
 
 for item in cmds.resourceManager(nf='deleteActive.png'):
-    cmds.resourceManager(s=(item, "{}{}".format(ICONS_FOLDER,item)))
+    cmds.resourceManager(s=(item, "{}{}".format(ICONS_FOLDER, item)))
