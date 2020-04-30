da9c714258a93ccdcc298505549afeb4e43cf5c2
adbrower <audreydb23@gmail.com>
2020-04-30 09:51:27 -0400

    lock attributes of the groups added SETTINGS_GRP

--- Adbrower/adb_rigModules/RigBase.py
+++ Adbrower/adb_rigModules/RigBase.py
@@ -1,5 +1,6 @@
 import pymel.core as pm
 
+from adbrower import lockAttr
 import adb_core.NameConv_utils as NC
 import adb_core.Class__AddAttr as adbAttr
 
@@ -12,16 +13,16 @@ class RigBase(object):
 
     def __init__(self, rigName = 'Audrey'):
         self.RIG_NAME = rigName
-        
+
         self._start()
 
     def _start(self):
         """
-        - Creates Rig Group 
+        - Creates Rig Group
         """
         self.createRigGroups(self.RIG_NAME)
-        
-    
+
+    @lockAttr()
     def createRigGroups(self, rigName):
         """
         @module_name : string. Name of the module
@@ -35,15 +36,9 @@ class RigBase(object):
         Returns: RIG_GRP, INPUT_GRP, OUTPUT_GRP
         """
         self.MAIN_RIG_GRP = pm.group(n='{}_Rig__GRP'.format(rigName), em=1)
-        self.VISIBILITY_GRP = pm.group(n='Visibility__GRP', em=1, parent=self.MAIN_RIG_GRP)
-        self.SPACES_GRP = pm.group(n='Space__GRP', em=1, parent=self.MAIN_RIG_GRP)
-        self.MODULES_GRP = pm.group(n='Module__GRP', em=1, parent=self.MAIN_RIG_GRP)
+        self.VISIBILITY_GRP = pm.group(n='{}_Visibility__GRP'.format(rigName), em=1, parent=self.MAIN_RIG_GRP)
+        self.SETTINGS_GRP = pm.group(n='{}_Settings__GRP'.format(rigName), em=1, parent=self.MAIN_RIG_GRP)
+        self.SPACES_GRP = pm.group(n='{}_Space__GRP'.format(rigName), em=1, parent=self.MAIN_RIG_GRP)
+        self.MODULES_GRP = pm.group(n='{}_Module__GRP'.format(rigName), em=1, parent=self.MAIN_RIG_GRP)
 
-        return self.MAIN_RIG_GRP, self.VISIBILITY_GRP, self.SPACES_GRP, self.MODULES_GRP
-
-
-
-    
-
-    
-    
+        return self.MAIN_RIG_GRP, self.VISIBILITY_GRP, self.SETTINGS_GRP, self.SPACES_GRP, self.MODULES_GRP, 
