56789d2d5fe72b764bc906f10e45375afd89c82b
adbrower <audreydb23@gmail.com>
2020-07-21 15:23:41 -0400

    added twist

--- Adbrower/adb_library/adb_utils/Script__PoseReader.py
+++ Adbrower/adb_library/adb_utils/Script__PoseReader.py
@@ -4,16 +4,32 @@ import pymel.core.datatypes as dt
 
 import adb_core.NameConv_utils as NC
 import adb_core.Class__Locator as Locator
+import adb_core.Class__AddAttr as adbAttr
 
 from adbrower import changeColor
 import adbrower
 
 adb = adbrower.Adbrower()
 
+getAxis = {
+    (1, 0, 0): "x",
+    (-1, 0, 0): "-x",
+    (0, 1, 0): "y",
+    (0, -1, 0): "-y",
+    (0, 0, 1): "z",
+    (0, 0, -1): "-z",
+}
+
+
 @changeColor('index', 3)
-def poseReader(name = '', driver='L__AutoClavicule_OUTPUT__GRP', target = 'L__AutoShoulder_Ik_Shoulder__JNT', upPostion = (0,10,0), targetPosition = (10,0,0)):
+def poseReader(name = '', driver='joint1__CTRL', target = 'joint2__CTRL', upPostion = (0,10,0), targetPosition = (10,0,0), twistPosition = (0,0,10), twist=False):
     """
     Create a Pose Reader
+
+    import adb_library.adb_utils.Script__PoseReader as PoseReader
+
+    PoseReader.PoseReader()
+
     """
     poseReaderGrp = pm.createNode('transform', name='{}_PoseReader__{}'.format(name, NC.GRP))
     pm.matchTransform(poseReaderGrp, driver, pos=True, rot=False)
@@ -26,7 +42,7 @@ def poseReader(name = '', driver='L__AutoClavicule_OUTPUT__GRP', target = 'L__Au
 
     [pm.matchTransform(loc, target, pos=True, rot=False) for loc in poseReaderLocs]
     [pm.parent(loc, poseReaderGrp) for loc in poseReaderLocs]
-    grp = adb.makeroot_func(targetPoseReader.locators)
+    targetGrp = adb.makeroot_func(targetPoseReader.locators)
 
     pm.move(upPoseReader.locators[0], upPostion, r=1, os=1, wd=1)
     pm.move(targetPoseReader.locators[0], targetPosition, r=1, os=1, wd=1)
@@ -34,7 +50,7 @@ def poseReader(name = '', driver='L__AutoClavicule_OUTPUT__GRP', target = 'L__Au
     pm.aimConstraint(targetPoseReader.locators[0], mainPoseReader.locators[0], weight=1, upVector=tuple(dt.Vector(upPostion).normal()), worldUpObject=upPoseReader.locators[0], worldUpType="object", offset=(0, 0, 0), aimVector=tuple(dt.Vector(targetPosition).normal()))
 
     pm.parent(poseReaderGrp, driver)
-    adb.matrixConstraint(target, grp)
+    adb.matrixConstraint(target, targetGrp)
 
     # add double Function
     adb.makeroot_func(mainPoseReader.locators)
@@ -56,9 +72,44 @@ def poseReader(name = '', driver='L__AutoClavicule_OUTPUT__GRP', target = 'L__Au
     mult.outputY >> mainPoseReaderOutput.locators[0].ry
     mult.outputZ >> mainPoseReaderOutput.locators[0].rz
 
-    poseReaderGrp.v.set(0)
+    ## add Twist
+    if twist:
+        twistPoseReaderGrp = pm.createNode('transform', name='{}_TwistPoseReader__{}'.format(name, NC.GRP))
+        pm.matchTransform(twistPoseReaderGrp, driver, pos=True, rot=False)
+        twistMainPoseReader = Locator.Locator.create(name='{}_TwistMainPoseReader__{}'.format(name, NC.LOC))
+        twistTargetPoseReader = Locator.Locator.create(name='{}_TwistTargetPoseReader__{}'.format(name, NC.LOC))
+        twistUpPoseReader = Locator.Locator.create(name='{}_TwistUpPoseReader__{}'.format(name, NC.LOC))
 
+        twistPoseReaderLocs = [twistMainPoseReader.locators, twistUpPoseReader.locators, twistTargetPoseReader.locators]
+        [pm.parent(loc, twistPoseReaderGrp) for loc in twistPoseReaderLocs]
+        pm.parent(twistPoseReaderGrp, poseReaderGrp)
+
+        [pm.matchTransform(loc, target, pos=True, rot=False) for loc in twistPoseReaderLocs]
+
+        pm.move(twistUpPoseReader.locators[0], targetPosition, r=1, os=1, wd=1)
+        pm.move(twistTargetPoseReader.locators[0], twistPosition, r=1, os=1, wd=1)
+
+        pm.aimConstraint(twistTargetPoseReader.locators[0], twistMainPoseReader.locators[0], weight=1, upVector=tuple(dt.Vector(targetPosition).normal()), worldUpObject=twistUpPoseReader.locators[0], worldUpType="object", offset=(0, 0, 0), aimVector=tuple(dt.Vector(twistPosition).normal()))
+        pm.parent(twistTargetPoseReader.locators[0], targetGrp)
+
+        mult.outputY >> twistPoseReaderGrp.ry
+        mult.outputZ >> twistPoseReaderGrp.rz
+
+    ## add Extra Attributes
+    mainLocAttr = adbAttr.NodeAttr(mainPoseReaderOutput.locators)
+    mainLocAttr.addAttr('bend', 0)
+    mainLocAttr.addAttr('twist', 0)
+    mainLocAttr.addAttr('bendSide', 0)
+
+    if twist:
+        twistMainPoseReader.locators[0].rx >>mainPoseReaderOutput.locators[0].twist
+    mainPoseReaderOutput.locators[0].ry >> mainPoseReaderOutput.locators[0].bendSide
+    mainPoseReaderOutput.locators[0].rz >>mainPoseReaderOutput.locators[0].bend
+
+
+    poseReaderGrp.v.set(0)
     return mainPoseReaderOutput.locators, upPoseReader.locators, targetPoseReader.locators
 
 
 
+# poseReader()
